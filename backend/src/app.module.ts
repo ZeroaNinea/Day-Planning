cd5b3113da04da269b605329d4919dbb9295aa85
Catgirl <zeroaninea@gmail.com>
2026-04-06 12:54:56 +0400

    auth added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot({ isGlobal?: true }), AuthModule],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
