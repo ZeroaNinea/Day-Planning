72d1b45a567353b9bf3f1a3463b3be77b2cb5217
Catgirl <zeroaninea@gmail.com>
2026-04-18 15:09:36 +0400

    adding the create path

--- backend/src/plan/plan.controller.ts
+++ backend/src/plan/plan.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Post } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+import { PlanService } from './plan.service';
 
 @Controller('plan')
-export class PlanController {}
+export class PlanController {
+  constructor(private pranService: PlanService) {}
+
+  @Post('create')
+  @UseGuards(AuthGuard('jwt'))
+  create() {}
+}
--- backend/src/plan/plan.module.ts
+++ backend/src/plan/plan.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { PlanController } from './plan.controller';
 import { PlanService } from './plan.service';
 
+import { Plan } from '../entities/plan.entity';
+import { User } from '../entities/user.entity';
+
 @Module({
+  imports: [
+    JwtModule.register({}),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forFeature([Plan, User]),
+  ],
   controllers: [PlanController],
   providers: [PlanService],
 })
