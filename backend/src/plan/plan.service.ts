b3611af634aeb2d8116d3769abdfee6a7e76b98e
Catgirl <zeroaninea@gmail.com>
2026-04-18 17:15:33 +0400

    the create function for user added

--- backend/src/plan/plan.service.ts
+++ backend/src/plan/plan.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Plan } from '../entities/plan.entity';
+
+import { Task } from '../../types/task.alias';
 
 @Injectable()
-export class PlanService {}
+export class PlanService {
+  constructor(
+    @InjectRepository(Plan)
+    private readonly planRepository: Repository<Plan>,
+  ) {}
+
+  create(userId: number, tasks: Task[]) {
+    const plan = this.planRepository.create({
+      user: { id: userId },
+      date: new Date().toISOString().split('T')[0],
+      tasks,
+    });
+
+    return this.planRepository.save(plan);
+  }
+}
