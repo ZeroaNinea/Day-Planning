7c71457516640e8f5125fbfc5b22322fc651f2a5
Catgirl <zeroaninea@gmail.com>
2026-04-19 20:03:32 +0400

    the plan.entity.ts updated

--- backend/src/entities/plan.entity.ts
+++ backend/src/entities/plan.entity.ts
@@ -26,6 +26,21 @@ export class Plan {
   @ManyToOne(() => User, (user) => user.id, { onDelete: 'CASCADE' })
   user!: User;
 
+  @Column()
+  isCompleted?: boolean;
+
+  @Column()
+  effort?: 'low' | 'medium' | 'high';
+
+  @Column()
+  duration?: number;
+
+  @Column()
+  start?: string;
+
+  @Column()
+  end?: string;
+
   @CreateDateColumn()
   createdAt!: Date;
 }
